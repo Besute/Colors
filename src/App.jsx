ba2e047532131aa2ab57fd6999d36880a119dab0
BORIS <boris200723@gmail.com>
2024-11-09 12:51:55 +0300

    Initilizate commit

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import "./css/style.css";
+
+function App() {
+  return (
+    <>
+      <div className="section-box__section section-box__section_1">
+        <p className="section-box__colorName">#00408C</p>
+        <h2 className="section-box__bigColorName">Dark Blue</h2>
+        <h3 className="section-box__colourPlatte">Colour Palette</h3>
+      </div>
+      <div className="section-box__section section-box__section_2">
+        <p className="section-box__colorName">#96ADD6</p>
+        <h2 className="section-box__bigColorName">Blue</h2>
+        <h3 className="section-box__colourPlatte">Colour Palette</h3>
+      </div>
+      <div className="section-box__section section-box__section_3">
+        <p className="section-box__colorName">#E85234</p>
+        <h2 className="section-box__bigColorName">Red</h2>
+        <h3 className="section-box__colourPlatte">Colour Palette</h3>
+      </div>
+      <div className="section-box__section section-box__section_4">
+        <p className="section-box__colorName">#F9B8AF</p>
+        <h2 className="section-box__bigColorName">Peach</h2>
+        <h3 className="section-box__colourPlatte">Colour Palette</h3>
+      </div>
+      <div className="section-box__section section-box__section_5">
+        <p className="section-box__colorName">#F2EEE9</p>
+        <h2 className="section-box__bigColorName">Cream</h2>
+        <h3 className="section-box__colourPlatte">Colour Palette</h3>
+      </div>
+      <div className="section-box__section section-box__section_6">
+        <p className="section-box__colorName">#F2D7D3</p>
+        <h2 className="section-box__bigColorName">Pink</h2>
+        <h3 className="section-box__colourPlatte">Colour Palette</h3>
+      </div>
+    </>
+  );
+}
+
+export default App;
